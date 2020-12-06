bb8b31482acf185385cc07f6bfbc1698dffe24c5
devenghadge13 <devenghadge13@gmail.com>
2020-12-06 14:57:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toes/SelectLanguageActivity.java
@@ -0,0 +1,14 @@
+package com.example.toes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectLanguageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_language);
+    }
+}
