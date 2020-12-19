4bd776e620d6804b87cb5e90ef439e035ada49c0
devenghadge13 <devenghadge13@gmail.com>
2020-12-19 15:26:22 +0530

    change in SignUpActivity

--- app/src/main/java/com/example/toes/SignUpActivity.java
+++ app/src/main/java/com/example/toes/SignUpActivity.java
@@ -2,13 +2,52 @@ package com.example.toes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SignUpActivity extends AppCompatActivity {
+    Calendar myCal = Calendar.getInstance();
+    EditText etDob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+        etDob = (EditText) findViewById(R.id.etDob);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCal.set(Calendar.YEAR, year);
+                myCal.set(Calendar.MONTH, month);
+                myCal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        etDob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SignUpActivity.this, date, myCal
+                        .get(Calendar.YEAR), myCal.get(Calendar.MONTH),
+                        myCal.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ENGLISH);
+
+        etDob.setText(sdf.format(myCal.getTime()));
     }
 }
