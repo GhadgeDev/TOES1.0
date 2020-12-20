8a261190fc609bccb501240b7b6b5093e9fb0851
devenghadge13 <devenghadge13@gmail.com>
2020-12-20 23:59:53 +0530

    updated

--- app/src/main/java/com/example/toes/LogoutDialog.java
+++ app/src/main/java/com/example/toes/LogoutDialog.java
@@ -3,18 +3,23 @@ package com.example.toes;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.DialogFragment;
+
 
 public class LogoutDialog extends DialogFragment {
     private TextView mTextSureMsg;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -22,7 +27,7 @@ public class LogoutDialog extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.logout_dialog,null);
 
-        builder.setView(view).setTitle("Logout")
+        builder.setView(view).setTitle("Logout").setIcon(R.drawable.icon)
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
